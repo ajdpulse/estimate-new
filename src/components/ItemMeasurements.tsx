e27bf4751f2c6953d98938d9e62d9dbc5c09925e
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-08-11 20:47:00 +0530

    Fix incomplete JSX expression syntax error

--- src/components/ItemMeasurements.tsx
+++ src/components/ItemMeasurements.tsx
@@ -432,4 +432,7 @@ const ItemMeasurements: React.FC<ItemMeasurementsProps> = ({
               </div>
               <div>
                 <span className="text-blue-700 font-medium">Category:</span>
-                <p className="text-blue-900">{currentItem.category ||
+                <p className="text-blue-900">{currentItem.category || '-'}</p>
+              </div>
+            </div>
+          </div>
