2eb04e651b2c91b4ec14f030c58ab6057522c846
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-09-29 00:29:54 +0530

    Updated EstimatePDFGenerator.tsx

--- src/components/generate-estimate/EstimatePDFGenerator.tsx
+++ src/components/generate-estimate/EstimatePDFGenerator.tsx
@@ -522,7 +522,7 @@ export const EstimatePDFGenerator: React.FC<EstimatePDFGeneratorProps> = ({
                         <span className="mr-2">:-</span>
                         <span>{estimateData.work.minor_head || '-'}</span>
                       </div>
-                      <div className="flex justify-center">
+                      <div className="flex">
                         <span className="w-40 font-medium">Service Head</span>
                         <span className="mr-2">:-</span>
                         <span>{estimateData.work.service_head || '-'}</span>
