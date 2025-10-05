5a979a3f1406b158340d368c5812e0695d80ea4c
Dpulse AI <shdpulseai@gmail.com>
2025-10-05 21:45:28 +0530

    Updated WorksRecapSheet.tsx

--- src/components/WorksRecapSheet.tsx
+++ src/components/WorksRecapSheet.tsx
@@ -165,7 +165,7 @@ const WorksRecapSheet: React.FC<WorksRecapSheetProps> = ({
     setSaved(false);
   };
 
-const handleSave = async () => {
+const handleSave = async () => {debugger;
   if (calculations && onSave) {
     onSave(calculations, taxes);
     setSaved(true);
